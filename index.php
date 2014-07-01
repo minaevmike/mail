9446263821b44098ecf2bd2dc6fb538644a433ec
minaevmike <minaev.mike@gmail.com>
2014-07-01 13:55:47 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<form action="get_msg.php" method="post">
+ <p>Login <input type="text" name="login" /></p>
+ <p>Password <input type="password" name="password" /></p>
+ <p>Id of bid <input type="text" name="id" /></p>
+ <p><input type="submit" /></p>
+</form>
+</body>
+</html>
